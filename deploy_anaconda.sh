11b14347118ebf24ddb3a010f2a85cd3198ff1f1
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2018-10-08 17:41:15 +0200

    moved deploy script on top
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- deploy_anaconda.sh
+++ deploy_anaconda.sh
@@ -5,11 +5,9 @@
 # >>> anaconda auth -c -n travis --max-age 307584000 --url https://anaconda.org/USERNAME/PACKAGENAME --scopes "api:write api:read"
 set -e
 
-echo "Converting conda package..."
-conda convert --platform all $HOME/miniconda2/conda-bld/linux-64/jsoncpp-*.tar.bz2 --output-dir conda-bld/
-
 echo "Deploying to Anaconda.org..."
-anaconda -t $ANACONDA_TOKEN upload conda-bld/**/jsoncpp-*.tar.bz2
+anaconda -t $ANACONDA_TOKEN upload $HOME/miniconda3/envs/test-env/conda-bld/linux-64/jsoncpp-*.tar.bz2
 
 echo "Successfully deployed to Anaconda.org."
 exit 0
+
